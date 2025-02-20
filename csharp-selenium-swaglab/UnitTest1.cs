06dc3228c52b5e6ade267fbae75d20ec26a8c6a2
idasabre <nhmsaber@gmail.com>
2025-02-20 18:11:35 +0800

    rename project file

---
+++ csharp-selenium-swaglab/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿namespace selenium_swaglab;
+
+public class TestRun
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
